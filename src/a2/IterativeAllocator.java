80bc2dbf41fc2f0be0102736c0421c34128a3021
joelyo <joe6278@hotmail.com>
2014-10-23 22:49:59 +1000

    6

--- src/a2/IterativeAllocator.java
+++ src/a2/IterativeAllocator.java
@@ -22,13 +22,18 @@ public class IterativeAllocator {
 			Set<Project> projects) {
 		// get set of potential paths
 		Set<List<Project>> paths = paths(donations, projects);
-		// check to see if a positive integer and path still exists that satisfies properties (1)-(3)
+		// check to see if a positive integer and path still exists that
+		// satisfies properties (1)-(3)
 		while (checkValidPaths(paths, donations)) {
 			// iterate over each donation and path to allocate funds if possible
 			for (Donation d : donations) {
 				for (List<Project> path : paths) {
-					if (checkProperties(Math.min(path.get(path.size()-1).neededFunds(), d.getUnspent()), path, donations)) {
-						allocateDonations(Math.min(path.get(path.size()-1).neededFunds(), d.getUnspent()), path, donations);
+					if (checkProperties(
+							Math.min(path.get(path.size() - 1).neededFunds(),
+									d.getUnspent()), path, donations)) {
+						allocateDonations(Math.min(path.get(path.size() - 1)
+								.neededFunds(), d.getUnspent()), path,
+								donations);
 					}
 				}
 			}
@@ -41,16 +46,17 @@ public class IterativeAllocator {
 		deallocateAll(projects);
 		return false;
 	}
-	
+
 	/**
 	 * returns a map that represents a graph of the given projects
 	 */
-	private static Map<Project, Set<Project>> graph(List<Donation> donations, Set<Project> projects) {
+	private static Map<Project, Set<Project>> graph(List<Donation> donations,
+			Set<Project> projects) {
 		Map<Project, Set<Project>> map = new HashMap<Project, Set<Project>>();
 		for (Project p1 : projects) {
 			// create key that represents a vertex
 			map.put(p1, new HashSet<Project>());
-			for (Project p2: projects) {
+			for (Project p2 : projects) {
 				if (!p1.equals(p2)) {
 					// check to see if an edge exists between p1 and p2
 					for (Donation d : donations) {
@@ -63,7 +69,7 @@ public class IterativeAllocator {
 		}
 		return map;
 	}
-	
+
 	/**
 	 * returns a map to track whether each project has been visited
 	 */
@@ -74,11 +80,13 @@ public class IterativeAllocator {
 		}
 		return map;
 	}
-	
+
 	/**
 	 * populates a list with a path between 2 projects found using DFS
 	 */
-	private static Boolean pathDFS(Map<Project, Set<Project>> map, List<Project> list, Project start, Project end, Map<Project, Boolean> visited) {
+	private static Boolean pathDFS(Map<Project, Set<Project>> map,
+			List<Project> list, Project start, Project end,
+			Map<Project, Boolean> visited) {
 		// mark start project as visited
 		visited.put(start, true);
 		// add start project to path list
@@ -95,45 +103,47 @@ public class IterativeAllocator {
 				if (pathDFS(map, list, p, end, visited)) {
 					return true;
 				}
-				// remove connecting project from list
-				list.remove(list.size()-1);
 			}
 		}
 		// remove start project from list
-		list.remove(list.size()-1);
+		list.remove(list.size() - 1);
 		return false;
 	}
-	
+
 	/**
 	 * returns a set of paths that can be traversed in the graph
 	 */
-	private static Set<List<Project>> paths(List<Donation> donations, Set<Project> projects) {
+	private static Set<List<Project>> paths(List<Donation> donations,
+			Set<Project> projects) {
 		// a set that contains paths
 		Set<List<Project>> paths = new HashSet<List<Project>>();
 		// get the graph of projects
 		Map<Project, Set<Project>> map = graph(donations, projects);
 		// iterate over every project pair and add a path to the set if found
-		for (Project p :  projects) {
+		for (Project p : projects) {
 			for (Project p2 : projects) {
 				List<Project> path = new ArrayList<Project>();
-				pathDFS(map, path, p, p2, visited(projects));
-				if (!path.isEmpty()) {
+				if (pathDFS(map, path, p, p2, visited(projects))) {
 					paths.add(path);
 				}
 			}
 		}
 		return paths;
 	}
-	
+
 	/**
-	 * returns true if a positive integer x and a path of n>0 distinct projects path satisfies properties (1)-(3)
+	 * returns true if a positive integer x and a path of n>0 distinct projects
+	 * path satisfies properties (1)-(3)
 	 */
-	private static boolean checkProperties(int x, List<Project> path, List<Donation> donations) {
-		// check that project path[n-1] is currently underfunded by at least x dollars and that x is a positive number
-		if (path.get(path.size()-1).neededFunds() < x || !(x > 0)) {
+	private static boolean checkProperties(int x, List<Project> path,
+			List<Donation> donations) {
+		// check that project path[n-1] is currently underfunded by at least x
+		// dollars and that x is a positive number
+		if (path.get(path.size() - 1).neededFunds() < x || !(x > 0)) {
 			return false;
 		}
-		// check that x dollars of funding could be allocated from the available donations to project path[0].
+		// check that x dollars of funding could be allocated from the available
+		// donations to project path[0].
 		int available = 0;
 		for (Donation d : donations) {
 			if (d.canBeUsedFor(path.get(0))) {
@@ -143,12 +153,14 @@ public class IterativeAllocator {
 		if (available < x) {
 			return false;
 		}
-		// check that for each i=n-2 to 0, x dollars from project path[i] could be transferred to project path[i+1]
-		for (int i = path.size()-2; i >= 0; i--) {
-			Set<Map.Entry<Donation, Integer>> it = path.get(i).getAllocations().entrySet();
+		// check that for each i=n-2 to 0, x dollars from project path[i] could
+		// be transferred to project path[i+1]
+		for (int i = path.size() - 2; i >= 0; i--) {
+			Set<Map.Entry<Donation, Integer>> it = path.get(i).getAllocations()
+					.entrySet();
 			int amount = 0;
 			for (Map.Entry<Donation, Integer> entry : it) {
-				if (entry.getKey().canBeUsedFor(path.get(i+1))) {
+				if (entry.getKey().canBeUsedFor(path.get(i + 1))) {
 					amount = amount + entry.getValue();
 				}
 			}
@@ -158,29 +170,34 @@ public class IterativeAllocator {
 		}
 		return true;
 	}
-	
+
 	/**
-	 * returns true if there exists a positive integer x and a path of n>0 distinct projects path satisfying properties (1)-(3)
+	 * returns true if there exists a positive integer x and a path of n>0
+	 * distinct projects path satisfying properties (1)-(3)
 	 */
-	private static boolean checkValidPaths(Set<List<Project>> paths, List<Donation> donations) {
-		boolean check = false;
+	private static boolean checkValidPaths(Set<List<Project>> paths,
+			List<Donation> donations) {
 		for (Donation d : donations) {
 			for (List<Project> path : paths) {
-				if (checkProperties(Math.min(path.get(path.size()-1).neededFunds(), d.getUnspent()), path, donations)) {
-					check = true;
+				if (checkProperties(
+						Math.min(path.get(path.size() - 1).neededFunds(),
+								d.getUnspent()), path, donations)) {
+					return true;
 				}
 			}
 		}
-		return check;
+		return false;
 	}
-	
+
 	/**
-	 * allocates x to the first project in the path and transfers x along the path of projects
+	 * allocates x to the first project in the path and transfers x along the
+	 * path of projects
 	 */
-	private static void allocateDonations(int x, List<Project> path, List<Donation> donations) {
+	private static void allocateDonations(int x, List<Project> path,
+			List<Donation> donations) {
 		// transfer x along the path of projects
-		for (int i=path.size()-2; i >= 0; i--) {
-			path.get(i+1).transfer(x, path.get(i));
+		for (int i = path.size() - 2; i >= 0; i--) {
+			path.get(i + 1).transfer(x, path.get(i));
 		}
 		// allocate x from available donations to path[0]
 		for (Donation d : donations) {
@@ -191,20 +208,27 @@ public class IterativeAllocator {
 			}
 		}
 	}
-	
+
 	/**
-	 * returns true if all projects are allocated
+	 * returns true if all projects are allocated and removes allocations of $0
+	 * from projects
 	 */
 	private static boolean allAllocated(Set<Project> projects) {
-		boolean check = true;
 		for (Project p : projects) {
 			if (!p.fullyFunded()) {
-				check = false;
+				return false;
+			}
+			// remove any allocations of $0
+			for (Map.Entry<Donation, Integer> allocation : p.getAllocations()
+					.entrySet()) {
+				if (allocation.getValue() == 0) {
+					p.deallocate(allocation.getKey(), 0);
+				}
 			}
 		}
-		return check;
+		return true;
 	}
-	
+
 	/**
 	 * deallocates all projects
 	 */
